d8c7d635783acfb578fc7495801e04f5e6df02fc
max1712 <maxence.de-la-bernardie@ensta-paristech.fr>
2018-03-16 15:18:51 +0100

    modif main

--- scripts/zonal/stage_dadp.jl
+++ scripts/zonal/stage_dadp.jl
@@ -1,4 +1,3 @@
-
 push!(LOAD_PATH, "..")
 
 using District, StochDynamicProgramming
@@ -23,21 +22,19 @@ nnodes = size(pb.nodes,1)
 
 # Build SP problems in each node
 build!(pb, xini, PriceInterface)
-# Select algorithm to solve global problem
-algo = DADP(pb, nsimu=1, nit=10)
+
 
 
 # Launch Gradient Descent!
 #= x0 = zeros(Float64, size(A, 1)*95) =#
 p = EDFPrice(ts).price[1:end-1]
 # initialization multiplicator
-x0 = p
+mul0 = p
 for i in 1:nnodes-1
-    x0 = vcat(x0, p)
+    mul0 = vcat(mul0, p)
 end
 
-if ALGO == "SDDP"
-    # TODO: currently we have to define sim before calling DADP to avoid side effect
+##################### SDDP #####
     nassess = 1
     sim = Simulator(pb, nassess, generation="reduction", nbins=30)
     params = District.get_sddp_solver()
@@ -46,10 +43,48 @@ if ALGO == "SDDP"
 
     pol = District.HereAndNowDP(sddp.bellmanfunctions)
     res = District.simulate(sim, pol)
-elseif ALGO == "DADP"
-    f, grad! = District.oracle(pb, algo)
-    gdsc = @time lbfgsb(f, grad!, x0; iprint=1, pgtol=1e-5, factr=0., maxiter=40)
+##################### SDDP #####
 
-    pol = District.DADPPolicy([algo.models[n.name].bellmanfunctions for n in pb.nodes])
-    res = District.simulate(sim, pol, )
+q = mean(mean(abs.(getflow(res.controls)),2),1)
+
+laplacian =  getlaplacian(pb.net.A , q[1,1,:])
+
+res = spectralclustering(laplacian, ncluster)
+
+
+# Coloring nodes
+membership = res.assignments
+
+for h in 1:nnodes
+    if membership[h] == 1
+        vec1 = hcat(vec1, pb.nodes[h])
+    elseif membership[h] == 2
+        vec2 = hcat(vec2, pb.nodes[h])
+    elseif membership[h] == 3
+        vec3 = hcat(vec3, pb.nodes[h])
+    end
 end
+
+
+
+
+z1 = Zone(ts, vec1, net1)
+z2 = Zone(ts, vec2)
+z3 = Zone(ts, vec3)
+
+pbreduced = Grid(ts, [z1, z2, z3], netreduced)
+
+# Build SP problems in each zone
+zonebuild!(pbreduced, xini, PriceInterface)
+
+
+# Select algorithm to solve global problem
+algo = DADP(pbreduced, nsimu=1, nit=10)
+
+
+f, grad! = District.oracle(pbreduced, algo)
+gdsc = @time lbfgsb(f, grad!, mul0; iprint=1, pgtol=1e-5, factr=0., maxiter=40)
+
+pol = District.DADPPolicy([algo.models[n.name].bellmanfunctions for n in pbreduced.nodes])
+res = District.simulate(sim, pol, )
+
