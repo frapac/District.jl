fa5b769239d154be12a590ab82f1df98530cff21
max1712 <maxence.dlb@gmail.com>
2018-03-23 17:37:33 +0100

    rendu non debuggue des fichiers avec zone

--- scripts/zonal/stage_dadp.jl
+++ scripts/zonal/stage_dadp.jl
@@ -5,8 +5,7 @@ using Lbfgsb, Optim
 
 srand(2713)
 
-ALGO = "SDDP"
-
+include("../graph.jl")
 
 # Time span
 ts = TimeSpan(180, 1)
@@ -14,8 +13,9 @@ ts = TimeSpan(180, 1)
 # Noise discretization
 nbins = 10
 
-# Build graph
+# Build grid
 #include("toygraph.jl")
+include("../problem.jl")
 pb, xini = twelvehouse(nbins=nbins)
 
 nnodes = size(pb.nodes,1)
@@ -25,65 +25,62 @@ build!(pb, xini, PriceInterface)
 
 
 
-# Launch Gradient Descent!
-#= x0 = zeros(Float64, size(A, 1)*95) =#
-p = EDFPrice(ts).price[1:end-1]
-# initialization multiplicator
-mul0 = p
-for i in 1:nnodes-1
-    mul0 = vcat(mul0, p)
-end
 
 ##################### SDDP #####
     nassess = 1
     sim = Simulator(pb, nassess, generation="reduction", nbins=30)
     params = District.get_sddp_solver()
-    params.max_iterations = 50
+    params.max_iterations = 1
     sddp = solve_SDDP(sim.model, params, 2, 1)
 
     pol = District.HereAndNowDP(sddp.bellmanfunctions)
     res = District.simulate(sim, pol)
 ##################### SDDP #####
 
+
+
+
+# Time-scenario mean flow on edges
 q = mean(mean(abs.(getflow(res.controls)),2),1)
 
+# Laplacian of incidence matrix
 laplacian =  getlaplacian(pb.net.A , q[1,1,:])
 
-res = spectralclustering(laplacian, ncluster)
+# Apply spectral clustering algorithm
+## Cluster number hardcoded
+ncluster = 3 
+clusterresult = spectralclustering(laplacian, ncluster)
 
+# Get node assignments to clusters
+membership = clusterresult.assignments 
 
-# Coloring nodes
-membership = res.assignments
+# Fill zones
+zones, netreduced = fillzones(pb, membership)
 
-for h in 1:nnodes
-    if membership[h] == 1
-        vec1 = hcat(vec1, pb.nodes[h])
-    elseif membership[h] == 2
-        vec2 = hcat(vec2, pb.nodes[h])
-    elseif membership[h] == 3
-        vec3 = hcat(vec3, pb.nodes[h])
-    end
-end
-
-
-
-# Build zones 
-z1 = Zone(ts, vec1, net1)
-z2 = Zone(ts, vec2)
-z3 = Zone(ts, vec3)
-
-pbreduced = Grid(ts, [z1, z2, z3], netreduced)
+# Build zonal grid
+pbreduced = ZonalGrid(ts, zones, netreduced)
 
 # Build SP problems in each zone
 zonebuild!(pbreduced, xini, PriceInterface)
 
 
-# Select algorithm to solve global problem
-algo = DADP(pbreduced, nsimu=1, nit=10)
 
-f, grad! = District.oracle(pbreduced, algo)
-gdsc = @time lbfgsb(f, grad!, mul0; iprint=1, pgtol=1e-5, factr=0., maxiter=40)
 
-pol = District.DADPPolicy([algo.models[n.name].bellmanfunctions for n in pbreduced.nodes])
-res = District.simulate(sim, pol, )
+##################### DADP #####
+	algo = DADP(pbreduced, nsimu=1, nit=10)
 
+	p = EDFPrice(ts).price[1:end-1]
+
+	# Initialization multiplicator
+	mul0 = p
+	for i in 1:nnodes-1
+	    mul0 = vcat(mul0, p)
+	end
+
+	# Compute f:mul and grad!:(mul, storage)
+	f, grad! = District.oracle(pbreduced, algo)
+	gdsc = @time lbfgsb(f, grad!, mul0; iprint=1, pgtol=1e-5, factr=0., maxiter=40)
+
+	pol = District.DADPPolicy([algo.models[n.name].bellmanfunctions for n in pbreduced.nodes])
+	res = District.simulate(sim, pol, )
+##################### DADP #####
--- src/model/zone.jl
+++ src/model/zone.jl
@@ -5,28 +5,38 @@
 ################################################################################
 
 
-mutable struct Zone <: AbstractNode
+mutable struct Zone <: AbstractNodalGrid
+	# name of House
+    name::Symbol
 	# Time span
     ts::AbstractTimeSpan
     # Nodes
     nodes::Vector{AbstractNode}
     # Nodes
     bordernodes::Vector{AbstractNode}
+    # Border nodes index
+    borderindex::Dict{AbstractNode, Float64}
     # Edges
     net::AbstractNetwork
     # SP model
     model::StochDynamicProgramming.SPModel
 end
 
-Zone(ts::AbstractTimeSpan) = Zone(ts, AbstractNode[], NoneNetwork())
+struct ZonalGrid <: AbstractGrid
+    # Time span
+    ts::AbstractTimeSpan
+    # Nodes
+    nodes::Vector{Zone}
+    # Edges
+    net::AbstractNetwork
+end
 
-nnodes(pb::Zone) = length(pb.nodes)
-narcs(pb::Zone) = pb.net.narcs
-ntimes(pb::Zone) = ntimesteps(pb.ts)
+Zone(ts::AbstractTimeSpan) = Zone(gensym(), ts, AbstractNode[], AbstractNode[], Dict(), NoneNetwork(), nothing)
 
+nnodes(pb::ZonalGrid) = sum(length.(zone.bordernodes for zone in pb.nodes))
 sizelambda(pb::Zone) = ntimes(pb) * size(bordernodes)
 
-swap!(zone::AbstractNode, exch::Array{Float64})
+function swap!(zone::Zone, exch::Array{Float64})
 	for (id,d) in zone.bordernodes
 		slambda = sizelambda(d)
         s1 = (id-1) * slambda + 1
@@ -35,7 +45,7 @@ swap!(zone::AbstractNode, exch::Array{Float64})
 	end
 end
 
-zonebuild!(grid::Grid, xini::Dict, Interface::Type=PriceInterface;
+function zonebuild!(grid::ZonalGrid, xini::Dict, Interface::Type=PriceInterface;
                 maxflow=6., tau=1.)
 	for zone in grid.nodes
 		build!(zone, xini[d in zone.nodes], PriceInterface)
@@ -45,7 +55,7 @@ zonebuild!(grid::Grid, xini::Dict, Interface::Type=PriceInterface;
 end
 
 
-build!(zone::AbstractNode, xini::Dict, Interface::Type=PriceInterface;
+function build!(zone::Zone, xini::Dict, Interface::Type=PriceInterface;
                 maxflow=6., tau=1.)
 	
 
@@ -70,4 +80,116 @@ build!(zone::AbstractNode, xini::Dict, Interface::Type=PriceInterface;
 
         build!(d, xini[d])
     end
+end
+
+
+
+function initvector(vector::Vector{Vector{Any}})
+	for i in 1:size(vector)
+		vector[i] = []
+	end
+end
+
+function fillzones(pb::Grid, membership::Vector{Int})
+	
+	# Total number of zones
+	nzones = maximum(membership)
+	# Total number of nodes
+	nbnodes = size(pb.nodes)
+
+ 	# Adjacency matrix
+	adjmatrix = getadjacence(pb.net.A)
+	
+	# Vector that associates a zone to its nodes
+	zonenode = Vector{Vector{AbstractNode}}(nzones)
+	# Vector that associates a zone to its border nodes
+	zoneborder = Vector{Vector{AbstractNode}}(nzones)
+	# Vector that associates a zone to a mapping between border nodes and their index in the node list 
+	borderindex = Vector{Dict{AbstractNode,Float64}}(nzones)
+
+	# Initiate vector of vectors with empty vectors
+	initvector(zonenode)
+	initvector(zoneborder)
+
+	
+
+	
+	# Fill the vectors zonenode, zoneborder and borderindex
+	for z in 1:nzones
+		# Index of a border node in the node list
+		bindex = 0.
+	    # fill nodes in zone z
+	    for i in 1:nbnodes
+	        # If node i belongs to zone z
+	        if membership[i] == z
+	            append!(zonenode[z], pb.nodes[i])
+	            bindex += 1    
+                # If node i has a neighbour out of its zone
+                for j in 1:nbnodes
+                    if (membership[i] != membership[j]) && (adjmatrix[i,j] == 1)
+                        append!(zoneborder[z], pb.nodes[i])
+                        get!(borderindex[z], pb.nodes[i], bindex)
+                        break
+                    end
+                end
+	        end
+	    end
+	end
+
+	# Instantiate zones 
+	zones = Vector{Zone}()
+
+	# Map between nodes and their index
+	index = Dict(pb.nodes[i]=> i for i in 1:nbnodes)
+	
+	# Build network of each zone and fill the vector zones
+	for z in 1:nzones
+		# Build network of zone z
+	    znodes = zonenode[z]
+	    nznodes = size(znodes)
+	    incidence = Vector{Vector{Float64}}(nznodes)
+	    initvector(incidence)
+
+	    for i in 1:nznodes-1
+			for j in (i+1):nznodes
+				if adjmatrix[index[znodes[i]],index[znodes[j]]] == 1
+					vect = zeros(Float64,nznodes)
+					vect[i] = 1
+					vect[j] = -1
+					append!(incidence, vect)
+				end
+			end
+		end
+
+	    append!(zones, Zone(ts, zonenode[z], zoneborder[z], borderindex[z], Network(ts, incidence)))
+	end
+
+
+	# Total number of border nodes
+	nbordernodes = sum(length.(zone.bordernodes for zone in zones))
+	# Vector of all border nodes
+	allbordernodes = Vector{AbstractNode}()
+	for zone in zones
+		for h in zone.bordernodes
+			append!(allbordernodes, h)
+		end
+	end
+
+	# Reduced incidence matrix 
+	incidence = Vector{Vector{Float64}}(nbordernodes)
+	initvector(incidence)
+
+	for i in 1:nbordernodes-1
+		for j in i+1:nbordernodes
+			if (membership[index[allbordernodes[i]]] != membership[index[allbordernodes[j]]]) &&  (adjmatrix[ index[allbordernodes[i]], index[allbordernodes[j]] ] == 1)
+				vect = zeros(Float64,nbordernodes)
+				vect[i] = 1
+				vect[j] = -1
+				append!(incidence, vect)
+			end
+		end
+	end
+
+
+	return zones, Network(ts, incidence)
 end
